106730be1e479410f888149fbed042c3db34424d
alfredo <alfredo.supanta.26@gmail.com>
2022-02-16 23:39:53 -0300

    maquetación terminada, queda hacer boton de eliminar, lista de suma de task

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+function App() {
+  return (
+    <main className="bg-dark">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-light text-center py-5">
+            <h1>
+              Todos Task <i className="bi bi-list-check"></i>
+            </h1>
+            <input className="mb-5 rounded w-100 py-2" type="text" name="newTask" id="newTask" />
+            <table className="table bg-dark">
+              <thead>
+              </thead>
+              <tbody>
+                <tr>
+                  <th className="text-light">
+                    lista de tareas
+                  </th>
+                </tr>
+                <tr>
+                  <th className="text-light">
+                    lista de tareas
+                  </th>
+                </tr>
+                <tr>
+                  <th className="text-light">
+                    lista de tareas
+                  </th>
+                </tr>
+                <tr>
+                  <th className="text-light">
+                    lista de tareas
+                  </th>
+                </tr>
+                <tr>
+                  <th className="text-light">
+                    lista de tareas
+                  </th>
+                </tr>
+                <tr>
+                  <th className="text-light">
+                    lista de tareas
+                  </th>
+                </tr>
+                <tr>
+                  <th className="text-light">
+                    lista de tareas
+                  </th>
+                </tr>
+                <tr>
+                  <th className="text-light">
+                    lista de tareas
+                  </th>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
